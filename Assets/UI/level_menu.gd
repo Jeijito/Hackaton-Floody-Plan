1d90406c7015f237bdcb7a7cf9c65702d948f61b
Jeijito <118863740+Jeijito@users.noreply.github.com>
2024-05-04 10:57:44 -0700

    Connections

--- Assets/UI/level_menu.gd
+++ Assets/UI/level_menu.gd
@@ -3,13 +3,12 @@ extends Control
 var level2_passed = false
 #func for the change of scene to level 1
 func _on_level_1_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Levels/level_p.tscn")
-
+	get_tree().change_scene_to_file("res://Scene/Levels/level_1.tscn")
 
 func _on_level_2_pressed():
 	if level2_passed:
 		print("playing level 2")
 	else:
-		get_tree().reload_scene() 
+		get_tree().reload_current_scene() 
 		
 		
