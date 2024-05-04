ff6acab80976f5132132e3edcdd80890089edce3
Tan Nguyen <nguyennhattan2707@gmail.com>
2024-05-04 13:15:17 -0700

    hihi

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -12,6 +12,7 @@ var time_jump_apex = 0.35
 var on_ground = false
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var jump_sounds = $jump_sounds
+var direction
 
 func _physics_process(delta):
 	gravity = (2 * jump_height) / pow(time_jump_apex, 2)
@@ -22,28 +23,37 @@ func _physics_process(delta):
 		on_ground = true
 	else:
 		on_ground = false
+		
+	if alive:
+		direction = Input.get_axis("move_left", "move_right")
+		
+	if direction > 0 and alive:
+		animated_sprite.flip_h = false
+	elif direction < 0 and alive:
+		animated_sprite.flip_h = true
 	
 	# Add the gravity.
 
 	# Handle jump.
 	if Input.is_action_pressed("move_left") and alive:
-		animated_sprite.flip_h = true
 		velocity.x = -speed
-		animated_sprite.play("run")
 	elif Input.is_action_pressed("move_right") and alive:
-		animated_sprite.flip_h = false
 		velocity.x = speed
 		animated_sprite.play("run")
+	elif Input.is_action_just_pressed("jump") and alive:
+		jump_sounds.play()
+		velocity.y = -jump_velocity
+		on_ground = false
 	else:
-		animated_sprite.play("idle")
 		velocity.x = 0
 		
-	if Input.is_action_just_pressed("jump") and alive:
-		if on_ground:
-			animated_sprite.play("jump")
-			jump_sounds.play()
-			velocity.y = -jump_velocity
-			on_ground = false
+	if is_on_floor() and alive:
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
 	
 
 	# Get the input direction and handle the movement/deceleration.
